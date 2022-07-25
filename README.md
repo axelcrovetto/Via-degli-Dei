6f2278bf96363f335be6cab77f50750d12538fb1
axelcrovetto <axelcrovetto2102@gmail.com>
2022-07-25 19:51:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-egn98r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-egn98r)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UsersComponent } from './users/users.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UserService } from './services/user.service';
+const routes: Routes = [
+  {
+    path: 'users',
+    pathMatch: 'full',
+    component: UsersComponent
+
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'users'
+  },
+  {
+    path: 'users/new',
+    component: UserDetailComponent
+  },
+  {
+    path: 'users/:id/edit',
+    component: UserDetailComponent
+  }
+  
+];
+
+@NgModule({
+  declarations: [
+    UsersComponent,
+    UserComponent,
+    UserDetailComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule,
+    FontAwesomeModule,
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
